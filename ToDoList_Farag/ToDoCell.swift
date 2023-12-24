847997b4c7b0ad828cc9cd8cc0f718087725f1f0
awab10h <awab10h@gmail.com>
2023-12-24 14:14:19 -0500

    ToDo app

---
+++ ToDoList_Farag/ToDoCell.swift
@@ -0,0 +1,35 @@
+//
+//  ToDoCell.swift
+//  ToDoList_Farag
+//
+//  Created by awab farag on 5/4/23.
+//
+
+import UIKit
+
+protocol ToDoCellDelegate: AnyObject {
+    func checkmarkTapped(sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+    
+    weak var delegate: ToDoCellDelegate?
+    @IBOutlet var isCompleteButton: UIButton!
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBAction func completeButtonTapped(_ sender: UIButton) {
+        delegate?.checkmarkTapped(sender: self)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ToDoList_Farag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList_Farag
+//
+//  Created by awab farag on 5/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
